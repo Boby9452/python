cdbd80132a78df9c4f6da98bdee8c5ca67ec7b8a
vijay Yadav <118282525+Boby9452@users.noreply.github.com>
2022-12-21 23:25:13 +0530

    Add files via upload

---
+++ swap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+  	int n,i,a;
+  	printf("enter a number");
+  	scanf("%d",&n);
+  	for(i=1,a=0;i<=n;i++)
+    {
+
+        a=a+i;
+    }
+ printf("%d",a);
+  }
